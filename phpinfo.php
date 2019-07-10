6bf9e074cf9cc7096bf114c4fdf49087cb527cb3
nu11secur1ty <venvaropt@gmail.com>
2019-07-10 13:30:13 +0300

    Update phpinfo.php

--- phpinfo.php
+++ phpinfo.php
@@ -1,1 +1,7 @@
-<?php phpinfo();?>
+<?php
+// Show all information, defaults to INFO_ALL
+phpinfo();
+// Show just the module information.
+// phpinfo(8) yields identical results.
+phpinfo(INFO_MODULES);
+?>
